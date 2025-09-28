f58afa7a5adf5457e6feb26f21b746839e527d92
oceanstuck <msamiclassified@gmail.com>
2025-09-27 17:00:20 -0700

    Add project files.

---
+++ FlowFunctions.cs
@@ -0,0 +1,217 @@
+﻿using p5rpc.flowscriptframework.interfaces;
+using p5rpc.flowutils.logging;
+using Reloaded.Mod.Interfaces;
+using RemixToolkit.Core.Configs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace p5rpc.flowutils;
+
+internal class FlowFunctions
+{
+    private IFlowFramework _flowFramework;
+    private Logger _logger;
+    private IModLoader _modLoader;
+
+    private Dictionary<int, int> confidantIds; 
+
+    public FlowFunctions(IFlowFramework flowFramework, IModLoader modLoader, ref Logger logger)
+    {
+        _flowFramework = flowFramework;
+        _logger = logger;
+        _modLoader = modLoader;
+    }
+
+    public void RegisterConfigReaders()
+    { 
+        _flowFramework.Register("IS_MOD_ENABLED", 1, () =>
+        {
+            var flowApi = _flowFramework.GetFlowApi();
+            var modId = flowApi.GetStringArg(0);
+            var isEnabled = _modLoader.GetAppConfig().EnabledMods.Contains(modId) ? 1 : 0;
+            flowApi.SetReturnValue(isEnabled);
+
+            return FlowStatus.SUCCESS;
+        });
+
+        _flowFramework.Register("GET_CONFIG_INT_VALUE", 2, () =>
+        {
+            var flowApi = _flowFramework.GetFlowApi();
+            var modId = flowApi.GetStringArg(0);
+            var configId = flowApi.GetStringArg(1);
+
+            object? value = null;
+            if (!TryGetR2ConfigValue(modId, configId, out value) && TryGetRemixConfigValue(modId, configId, out value))
+            {
+                _logger.WriteLog(LogLevel.ERROR, $"Failed to read config file for {modId}.");
+                throw new Exception($"Failed to read config file for {modId}.");
+            }
+            flowApi.SetReturnValue((int)value);
+
+            return FlowStatus.SUCCESS;
+        });
+
+        _flowFramework.Register("GET_CONFIG_FLOAT_VALUE", 2, () =>
+        {
+            var flowApi = _flowFramework.GetFlowApi();
+            var modId = flowApi.GetStringArg(0);
+            var configId = flowApi.GetStringArg(1);
+
+            object? value = null;
+            if (!TryGetR2ConfigValue(modId, configId, out value, true) && TryGetRemixConfigValue(modId, configId, out value, true))
+            {
+                _logger.WriteLog(LogLevel.ERROR, $"Failed to read config file for {modId}.");
+                throw new Exception($"Failed to read config file for {modId}.");
+            }
+            flowApi.SetReturnValue((float)value);
+
+            return FlowStatus.SUCCESS;
+
+        });
+    }
+
+    public void RegisterMiscFunctions()
+    {
+        confidantIds = new Dictionary<int, int>()
+        {
+            [11] = 27,
+            [14] = 28,
+            [15] = 29,
+            [16] = 30,
+            [18] = 31,
+            [33] = 34
+        };
+
+        _flowFramework.Register("CMM_GET_IN_USE_ID", 1, () =>
+        {
+            var flowApi = _flowFramework.GetFlowApi();
+            var id = flowApi.GetIntArg(0);
+
+
+        });
+    }
+
+    public void RegisterCustomSaveDataHandlers()
+    {
+
+    }
+
+    private bool TryGetR2ConfigValue(string modId, string configId, out object? configValue, bool isFloat = false)
+    {
+        configValue = null;
+        var configPath = Path.Combine(_modLoader.GetModConfigDirectory(modId), "Config.json");
+        if (!File.Exists(configPath))
+        {
+            _logger.WriteLog(LogLevel.INFO, $"Couldn't find R2 config file for {modId}.");
+            return false;
+        }
+
+        using (JsonDocument r2Config = JsonDocument.Parse(File.ReadAllText(configPath)))
+        {
+            var configItem = r2Config.RootElement.GetProperty(configId);
+            switch (configItem.ValueKind)
+            {
+                case JsonValueKind.True:
+                    configValue = 1;
+                    break;
+                case JsonValueKind.False:
+                    configValue = 0;
+                    break;
+                case JsonValueKind.Number:
+                    if (isFloat && configItem.TryGetDouble(out var floatVal)) { configValue = floatVal; }
+                    else if (configItem.TryGetInt32(out var intVal)) { configValue = intVal; }
+                    else
+                    {
+                        _logger.WriteLog(LogLevel.ERROR, $"Failed to parse config value {configId} in {modId}.");
+                        throw new ArgumentException($"Failed to parse config value {configId} in {modId}.");
+                    }
+                    break;
+                default:
+                    _logger.WriteLog(LogLevel.ERROR, $"Config value {configId} in {modId} is unsupported type: {configItem.ValueKind.ToString()}.");
+                    throw new ArgumentException($"Config value {configId} in {modId} is unsupported type: {configItem.ValueKind.ToString()}.");
+            }
+        }
+        return true;
+    }
+
+    private bool TryGetRemixConfigValue(string modId, string configId, out object? configValue, bool isFloat = false)
+    {
+        configValue = null;
+
+        var configPath = Path.Combine(_modLoader.GetModConfigDirectory(modId), "ReMIX", "Config", "data.yaml");
+        string schemaPath = Path.Combine(_modLoader.GetDirectoryForModId(modId), "ReMIX", "Config", "config.yaml");
+        if (!File.Exists(schemaPath))
+        {
+            _logger.WriteLog(LogLevel.ERROR, $"Couldn't find ReMIX schema file for {modId}.");
+            return false;
+        }
+        if (!File.Exists(configPath))
+        {
+            _logger.WriteLog(LogLevel.ERROR, $"Couldn't find ReMIX config file for {modId}.");
+            return false;
+        }
+
+        var yamlDeserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().WithNamingConvention(UnderscoredNamingConvention.Instance).Build();
+
+        var remixConfig = yamlDeserializer.Deserialize<Dictionary<string, object>>(File.ReadAllText(configPath));
+        if (remixConfig == null)
+        {
+            _logger.WriteLog(LogLevel.ERROR, $"Failed to deserialize ReMIX config for {modId}.");
+            throw new Exception($"Failed to deserialize ReMIX config for {modId}.");
+        }
+
+        var schemaParser = new Parser(new StringReader(schemaPath));
+        schemaParser.Consume<StreamStart>();
+        schemaParser.Consume<DocumentStart>();
+        schemaParser.Consume<MappingStart>();
+
+        ConfigSetting? configSchema = null;
+        while (schemaParser.TryConsume<Scalar>(out var section))
+        {
+            if (section.Value == "settings")
+            {
+                configSchema = yamlDeserializer.Deserialize<ConfigSetting[]>(schemaParser).Where(x => x.Id == configId).First();
+                break;
+            }
+            else { schemaParser.SkipThisAndNestedEvents(); }
+        }
+
+        if (configSchema == null)
+        {
+            _logger.WriteLog(LogLevel.ERROR, $"Failed to deserialize ReMIX schema for {modId}.");
+            throw new Exception($"Failed to deserialize ReMIX schema for {modId}.");
+        }
+
+        if (remixConfig.TryGetValue(configSchema.Id, out configValue))
+        {
+            if (configValue == null) { configValue = configSchema.GetDefaultValue(); }
+
+            var configType = configSchema.GetPropertyType();
+            if (configType == typeof(string))
+            {
+                _logger.WriteLog(LogLevel.ERROR, $"Config value {configId} in {modId} is unsupported type: {configType.ToString()}.");
+                throw new ArgumentException($"Config value {configId} in {modId} is unsupported type: {configType.ToString()}.");
+            }
+            else if (isFloat)
+            {
+                if (configType == typeof(double)) { _logger.WriteLog(LogLevel.WARNING, $"{configId} in {modId} is of type double. Some precision may be lost."); }
+                configValue = Convert.ChangeType(configValue, configType);
+            }
+            else
+            {
+                configValue = configType.IsEnum ? Convert.ToInt32(configValue) : Convert.ChangeType(configValue, configType);
+                if (configType == typeof(bool)) { configValue = (bool)configValue ? 1 : 0; }
+            }
+        }
+        return true;
+    }
+}
---
+++ RemixModels/ConfigSetting.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace RemixToolkit.Core.Configs.Models;
+
+public class ConfigSetting
+{
+    private static readonly AssemblyName SHARED_NAME = new(Guid.NewGuid().ToString());
+    private static readonly AssemblyBuilder SHARED_ASS = AssemblyBuilder.DefineDynamicAssembly(SHARED_NAME, AssemblyBuilderAccess.RunAndCollect);
+    private static readonly ModuleBuilder SHARED_MOD = SHARED_ASS.DefineDynamicModule(SHARED_NAME.Name!);
+    private static readonly Dictionary<string, Type> DEFINED_ENUMS = [];
+
+    private const string DEFAULT_TYPE = "bool";
+
+    /// <summary>
+    /// ID of setting, used to retrieve setting value.
+    /// </summary>
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Type of setting.
+    /// </summary>
+    public string Type { get; set; } = DEFAULT_TYPE;
+
+    /// <summary>
+    /// (Optional) Display name for setting.
+    /// </summary>
+    public string? Name { get; set; } = null;
+
+    /// <summary>
+    /// (Optional) Setting category.
+    /// </summary>
+    public string Category { get; set; } = string.Empty;
+
+    /// <summary>
+    /// (Optional) Setting description.
+    /// </summary>
+    public string? Description { get; set; } = null;
+
+    /// <summary>
+    /// (Optional) Predefined default value for setting, else setting type default.
+    /// </summary>
+    public string? Default { get; set; } = null;
+
+    /// <summary>
+    /// (Optional) List to include setting value in.
+    /// </summary>
+    public string? List { get; set; } = null;
+
+    /// <summary>
+    /// (Optional / bool) Value to use if bool state is true.
+    /// </summary>
+    public string? ValueOn { get; set; } = null;
+
+    /// <summary>
+    /// (Optional / bool) Value to use if bool state is false.
+    /// </summary>
+    public string? ValueOff { get; set; } = null;
+
+    /// <summary>
+    /// (Required / enum) Values of an enum setting.
+    /// </summary>
+    public string[]? Choices { get; set; }
+
+    public Type GetPropertyType()
+        => Type switch
+        {
+            "bool" or "toggle" => typeof(bool),
+            "string" or "text" => typeof(string),
+            "enum" or "choice" => GetEnumType(),
+            "int" or "number" => typeof(int),
+
+            "byte" => typeof(byte),
+            "short" => typeof(short),
+            "float" => typeof(float),
+            "double" => typeof(double),
+            _ => throw new NotImplementedException($"Unknown setting type: {Type}"),
+        };
+
+    public object? GetDefaultValue()
+    {
+        var type = GetPropertyType();
+
+        if (Type == "enum" || Type == "choice")
+        {
+            var enumValues = Enum.GetNames(type);
+
+            // Gets predefined default using its index in list of choices
+            // applied to the enum's list of values.
+            if (Default != null) return enumValues[Array.IndexOf(Choices!, Default)];
+
+            // If no default is set, default to first value.
+            return Enum.GetNames(type).First();
+        }
+
+        // No default value set, use default value of prop type.
+        if (Default == null)
+        {
+            if (type.IsValueType) return Activator.CreateInstance(type);
+
+            return null;
+        }
+
+        return Convert.ChangeType(Default, type);
+    }
+
+    private Type GetEnumType()
+    {
+        if (DEFINED_ENUMS.TryGetValue(Id, out var type)) return type;
+
+        if (Choices == null || Choices.Length == 0) throw new Exception($"\"Choice\" setting must have at least 1 choice: {Id}");
+
+        var enumName = $"enum_{DEFINED_ENUMS.Count}";
+        var enumBuilder = SHARED_MOD.DefineEnum(enumName, TypeAttributes.Public, typeof(int));
+
+        for (int i = 0; i < Choices.Length; i++)
+        {
+            var enumValue = enumBuilder.DefineLiteral($"option_{i}", i);
+            enumValue.SetCustomAttribute(new DisplayAttributeBuilder(Choices[i]));
+        }
+
+        var enumType = enumBuilder.CreateType();
+        DEFINED_ENUMS.Add(Id, enumType);
+        return enumType;
+    }
+
+    private class DisplayAttributeBuilder(string name)
+        : CustomAttributeBuilder(DISP_ATTR_CTOR, [], [DISP_ATTR_PROP_NAME], [name])
+    {
+        private static readonly Type DISP_ATTR_TYPE = typeof(DisplayAttribute);
+        private static readonly ConstructorInfo DISP_ATTR_CTOR = DISP_ATTR_TYPE.GetConstructor([])!;
+        private static readonly PropertyInfo DISP_ATTR_PROP_NAME = DISP_ATTR_TYPE.GetProperty("Name")!;
+    }
+}
