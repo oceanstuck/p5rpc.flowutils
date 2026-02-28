c2d23cbd1574fe02283cfc66c597dac1eab13f88
oceanstuck <msamiclassified@gmail.com>
2026-02-27 18:13:47 -0800

    idk

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -1,5 +1,5 @@
 Split-Path $MyInvocation.MyCommand.Path | Push-Location
 [Environment]::CurrentDirectory = $PWD
 
-.\p5rpc.flowutils\Publish.ps1 -ReadmePath ..\README.md
-.\p5rpc.flowutils.customsavedata\Publish.ps1 -ReadmePath ..\..\README.md
+.\p5rpc.flowutils\Publish.ps1 -ReadmePath .\README.md
+.\p5rpc.flowutils.customsavedata\Publish.ps1 -ReadmePath .\README.md
