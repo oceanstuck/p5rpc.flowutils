c99d0f575a8b9fe1ca96d1e4e67702871025ac0b
oceanstuck <msamiclassified@gmail.com>
2026-02-27 22:16:46 -0800

    oops

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -2,7 +2,7 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 [Environment]::CurrentDirectory = $PWD
 
 $readmePath = ./README.md
-$publishPath = Publish/ToUpload
+$publishPath = ./Publish/ToUpload
 
 ./p5rpc.flowutils/Publish.ps1 -ReadmePath $readmePath -PublishOutputDir $publishPath
 
