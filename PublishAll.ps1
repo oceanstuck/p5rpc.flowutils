ae75fff0ee954cc4c4630ea7bfef06bf8cb76b32
oceanstuck <msamiclassified@gmail.com>
2026-02-27 16:19:34 -0800

    set readme path for PublishAll

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -1,2 +1,2 @@
-.\p5rpc.flowutils\Publish.ps1 -ProjectPath .\p5rpc.flowutils\p5rpc.flowutils.csproj
-.\p5rpc.flowutils.customsavedata\Publish.ps1 -ProjectPath .\p5rpc.flowutils.customsavedata\p5rpc.flowutils.customsavedata.csproj
+.\p5rpc.flowutils\Publish.ps1 -ProjectPath .\p5rpc.flowutils\p5rpc.flowutils.csproj -ReadmePath ..\README.md
+.\p5rpc.flowutils.customsavedata\Publish.ps1 -ProjectPath .\p5rpc.flowutils.customsavedata\p5rpc.flowutils.customsavedata.csproj -ReadmePath ..\README.md
