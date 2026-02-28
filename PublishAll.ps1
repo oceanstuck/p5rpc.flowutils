ab9041ea1ad824857b25bec8c254877a23e4b0c4
oceanstuck <msamiclassified@gmail.com>
2026-02-27 17:59:51 -0800

    why is it inserting an extra folder wtf is it doing

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -2,8 +2,4 @@ Split-Path $MyInvocation.MyCommand.Path | Push-Location
 [Environment]::CurrentDirectory = $PWD
 
 .\p5rpc.flowutils\Publish.ps1 -ReadmePath ..\README.md
-
-Split-Path $MyInvocation.MyCommand.Path | Push-Location
-[Environment]::CurrentDirectory = $PWD
-
-.\p5rpc.flowutils.customsavedata\Publish.ps1 -ReadmePath ..\README.md
+.\p5rpc.flowutils.customsavedata\Publish.ps1 -ReadmePath ..\..\README.md
