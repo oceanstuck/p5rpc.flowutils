a0fdcd1953d7e4d18d5ae6fbb5d54665c5210ae3
oceanstuck <msamiclassified@gmail.com>
2026-02-27 17:20:24 -0800

    workflows FAFO

--- PublishAll.ps1
+++ PublishAll.ps1
@@ -1,5 +1,5 @@
 Split-Path $MyInvocation.MyCommand.Path | Push-Location
 [Environment]::CurrentDirectory = $PWD
 
-.\p5rpc.flowutils\Publish.ps1 -ProjectPath .\p5rpc.flowutils\p5rpc.flowutils.csproj -ReadmePath README.md -ChangelogPath p5rpc.flowutils\CHANGELOG.MD
-.\p5rpc.flowutils.customsavedata\Publish.ps1 -ProjectPath .\p5rpc.flowutils.customsavedata\p5rpc.flowutils.customsavedata.csproj -ReadmePath README.md -ChangelogPath p5rpc.flowutils\CHANGELOG.MD
+.\p5rpc.flowutils\Publish.ps1 -ReadmePath README.md
+.\p5rpc.flowutils.customsavedata\Publish.ps1 -ReadmePath README.md
